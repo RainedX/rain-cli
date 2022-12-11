fc5be7a50b205cb43eb1735bb466637a332e2b3d
rained <1365226650@qq.com>
2022-12-11 11:16:55 +0800

    feat: 新增wds

--- bin/rain-cli.js
+++ bin/rain-cli.js
@@ -1,32 +1,24 @@
 #!/usr/bin/env node
 
-const { program } = require('commander');
-const checkNodeVersion = require('../lib/checkNodeVersion');
-const pkg = require('../package.json');
-const startServer = require('../lib/start/startServer');
-const build = require('../lib/build/build');
-const MIN_NODE_VSRSION = '16.0.0';
+const { program } = require('commander')
+const checkNodeVersion = require('../lib/checkNodeVersion')
+const pkg = require('../package.json')
+const startServer = require('../lib/start/startServer')
+const build = require('../lib/build/build')
+const MIN_NODE_VSRSION = '14.0.0'
 
-(async () => {
+;(async () => {
   try {
     if (!checkNodeVersion(MIN_NODE_VSRSION)) {
       throw new Error('Please upgrade your node version to v' + MIN_NODE_VSRSION)
     }
 
-    program.version(pkg.version);
+    program.version(pkg.version)
 
-    program
-          .command('start')
-          .description('start imooc-build server')
-          .allowUnknownOption()
-          .action(startServer);
-    program
-          .command('build')
-          .description('build project by imooc-build')
-          .allowUnknownOption()
-          .action(build);
+    program.command('start').description('start imooc-build server').allowUnknownOption().action(startServer)
+    program.command('build').description('build project by imooc-build').allowUnknownOption().action(build)
 
-    program.parse(process.argv);
+    program.parse(process.argv)
   } catch (error) {
     console.log(error.message)
   }
